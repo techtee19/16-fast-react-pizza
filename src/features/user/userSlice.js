018e4430442b017d30932cd8cdcdc147b5823197
techtee19 <techtee19@gmail.com>
2025-10-07 21:37:59 +0100

    feat: implement Redux for user state management, update Username component to display dynamic username, and configure store

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -25,7 +25,7 @@ async function fetchAddress() {
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  username: '',
+  username: 'nifemi',
 };
 
 const userSlice = createSlice({
